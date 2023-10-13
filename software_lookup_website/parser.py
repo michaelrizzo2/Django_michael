9f066f114f0f1d335f7d4b2dc180ef75069116f4
Michael Rizzo <m_rizzo@live.com>
2023-10-13 14:40:23 -0400

    parser is now split up correctly.

--- software_lookup_website/parser.py
+++ software_lookup_website/parser.py
@@ -4,11 +4,14 @@ with open('software_lookup_website/json_small.json', 'r') as fcc_file:
     for entry in data:
         dictionary_for_data={"Sws":"Fenix","Details":"description","Domain Instance":"domain","Core Files":"sws_info","Applications":"sws_info"}
         for key,value in dictionary_for_data.items():
-            if key == "Core Files":
-                print(entry[value]["Apps"])
-            if key == "Applications":
-                print(entry[value]["Core Files"])
-            #This will call the entries without nested dictionaries
+            if key in ["Core Files","Applications"]:
+                if key == "Core Files":
+                    print("Core Files")
+                    print(entry[value]["Core Files"])
+                if key == "Applications":
+                    print("Applications")
+                    print(entry[value]["Apps"])
             else:
                 print(entry[value])
+            #This will call the entries without nested dictionaries
         
