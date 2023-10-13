eeb19e1d635785fea01a3cbda041d977518d882c
Michael Rizzo <m_rizzo@live.com>
2023-10-13 14:15:22 -0400

    made json_small.json not use git lfs.

--- software_lookup_website/parser.py
+++ software_lookup_website/parser.py
@@ -1,5 +1,7 @@
 import json 
 with open('software_lookup_website/json_small.json', 'r') as fcc_file:
     data=json.load(fcc_file)
-    for i in data:
-        print(i['sws_info'])
+    for entry in data:
+        columns_needed=["Sws","Details","Domain Instance","Core Files","Applications"]
+        
+        print(entry['sws_info'])
