c963fa19e075067783cf1659ce1f3fb4a3f9ee83
Michael Rizzo <m_rizzo@live.com>
2023-10-13 14:35:58 -0400

    parser had been finished. neet to get input to parser.

--- software_lookup_website/parser.py
+++ software_lookup_website/parser.py
@@ -2,6 +2,13 @@ import json
 with open('software_lookup_website/json_small.json', 'r') as fcc_file:
     data=json.load(fcc_file)
     for entry in data:
-        columns_needed=["Sws","Details","Domain Instance","Core Files","Applications"]
+        dictionary_for_data={"Sws":"Fenix","Details":"description","Domain Instance":"domain","Core Files":"sws_info","Applications":"sws_info"}
+        for key,value in dictionary_for_data.items():
+            if key == "Core Files":
+                print(entry[value]["Apps"])
+            if key == "Applications":
+                print(entry[value]["Core Files"])
+            #This will call the entries without nested dictionaries
+            else:
+                print(entry[value])
         
-        print(entry['sws_info'])
