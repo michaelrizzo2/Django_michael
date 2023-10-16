1aa6b6506d54f7cd218dbe2674291d6e53255101
Michael Rizzo <m_rizzo@live.com>
2023-10-16 16:11:37 -0400

    Filled in tabel with proper results.

--- software_lookup_website/parser.py
+++ software_lookup_website/parser.py
@@ -1,6 +1,7 @@
 import json 
 with open('software_lookup_website/json_small.json', 'r') as fcc_file:
     data=json.load(fcc_file)
+    print(len(data))
     for entry in data:
         dictionary_for_data={"Sws":"Fenix","Details":"description","Domain Instance":"domain","Core Files":"sws_info","Applications":"sws_info"}
         for key,value in dictionary_for_data.items():
