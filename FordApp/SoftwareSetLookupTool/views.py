fc33174ba1785250beab765524c37e43f24d4307
Michael Rizzo <m_rizzo@live.com>
2023-10-19 13:02:52 -0400

    changed build url form to send build url

--- FordApp/SoftwareSetLookupTool/views.py
+++ FordApp/SoftwareSetLookupTool/views.py
@@ -3,6 +3,7 @@ def login(request):
     return render(request, "login.html")
 
 def buildurl(request):
+    print(request.POST)
     return render(request, "buildurl.html")
 
 def lookuptable(request):
