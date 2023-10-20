4b5a63d01a6808e7dbed394a13916dd48017d6d1
Michael Rizzo <m_rizzo@live.com>
2023-10-20 11:27:25 -0400

    i cannot get any data sent to the web pages. this app will never get finished.

--- FordApp/SoftwareSetLookupTool/views.py
+++ FordApp/SoftwareSetLookupTool/views.py
@@ -3,9 +3,7 @@ def login(request):
     return render(request, "login.html")
 
 def buildurl(request):
-    print(request)
     return render(request, "buildurl.html")
 
 def lookuptable(request):
-    print(request)
     return render(request, "lookuptable.html")
