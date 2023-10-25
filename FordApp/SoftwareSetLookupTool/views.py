d64809e28cb21a5a239fe44076440aafb1680d5a
Michael Rizzo <m_rizzo@live.com>
2023-10-25 17:05:46 -0400

    Latest Copy. Use this for demo for nexty tuesday.

--- FordApp/SoftwareSetLookupTool/views.py
+++ FordApp/SoftwareSetLookupTool/views.py
@@ -3,14 +3,11 @@ from FordApp.forms import BuildUrlForm,LoginForm
 def login(request):
     if request.method=="GET":
         form=LoginForm()
-    return render(request, 'login.html', {
-'form': form,
-})
+    return render(request, 'login.html', {'form': form })
 
 def buildurl(request):
     if request.method =="GET":
         form=BuildUrlForm()
-        print(form)
     return render(request,"buildurl.html",{'form': form})
 
 def lookuptable(request):
