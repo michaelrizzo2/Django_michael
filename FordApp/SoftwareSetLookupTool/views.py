7501b268fd3f87c08ff9a433a3f322c8d8b3c0cc
Michael Rizzo <m_rizzo@live.com>
2023-10-25 15:14:06 -0400

    getting data to be sent to lookup table page.

--- FordApp/SoftwareSetLookupTool/views.py
+++ FordApp/SoftwareSetLookupTool/views.py
@@ -8,12 +8,7 @@ def login(request):
 })
 
 def buildurl(request):
-    if request.method=="GET":
-        form=BuildUrlForm()
-    else:
-        form=BuildUrlForm(request.POST)
-        if form.is_valid():
-            build_url=form.cleaned_data['build_url']
+    form=BuildUrlForm()
     return render(request,"buildurl.html",{'form': form})
 
 def lookuptable(request):
