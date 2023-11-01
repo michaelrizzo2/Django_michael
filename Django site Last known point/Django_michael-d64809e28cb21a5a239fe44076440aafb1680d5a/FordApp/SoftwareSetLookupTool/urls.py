9e104cc4c87d314df5757dff289abf9051c5bea4
Michael Rizzo <m_rizzo@live.com>
2023-11-01 11:25:23 -0400

    moved backup to home directory.

--- Django site Last known point/Django_michael-d64809e28cb21a5a239fe44076440aafb1680d5a/FordApp/SoftwareSetLookupTool/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("", views.login, name="login"),
-    path("buildurl/", views.buildurl, name="buildurl"),
-    path("lookuptable/", views.lookuptable, name="lookuptable"),
-]
